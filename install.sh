81eff8b3e31b6095f0c582025c7adb139f430aae
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-06-01 21:05:20 +0600

    updated

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
-#
-#    Copyright 2007-2009 Canonical Ltd.
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License as
-#  published by the Free Software Foundation; either version 2 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
 
 LIBFILES="main.py raw_write.py raw_format.py mountutils.py window.ui"
 
@@ -39,9 +24,5 @@ else
 
     for item in $LIBFILES; do
         cp lib/$item /usr/lib/flash-drive-tool/
-    done
-
-    for item in $DATAFILES; do
-        cp share/flash-drive-tool/$item /usr/share/flash-drive-tool/
     done
 fi
